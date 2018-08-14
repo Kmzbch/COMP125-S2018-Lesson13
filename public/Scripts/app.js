120129486d1a82284af7d0e8d18f9fa2290bf9b2
Kmzbc <Kmizubuc@my.centennialcollege.ca>
2018-08-13 22:35:59 -0400

    modified some sources

---
+++ public/Scripts/app.js
@@ -0,0 +1,6 @@
+(function () {
+    function Start() {
+        console.log(`%c App Started...`, "color: blue; font-size: 30px; font-weight: bold;");
+    }
+    window.addEventListener("load", Start);
+})();
