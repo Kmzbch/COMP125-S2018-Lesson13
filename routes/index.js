7ff4bb2a410d318ffd4002b5205d4965cf66722a
Kmzbc <Kmizubuc@my.centennialcollege.ca>
2018-08-13 23:34:19 -0400

    updated partials and content

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,25 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", {
+router.get("/", function (req, res, next) {
+  res.render("content/index", {
     title: "Express Demo"
   });
 });
 
+
+
 /* GET about page */
-router.get("/about", function(req, res, next) {
-  res.render("about", {
-    title: "About express",
+router.get("/about", function (req, res, next) {
+  res.render("content/about", {
+    title: "About Express",
     author: "Kei Mizubuchi"
   });
 });
 
 /* GET contact page */
-router.get("/contact", function(req, res, next) {
-  res.render("contact", {
+router.get("/contact", function (req, res, next) {
+  res.render("content/contact", {
     title: "Contact Me",
     authors: ["Tom", "Mark", "Luke", "Jaden", "John"]
   });
